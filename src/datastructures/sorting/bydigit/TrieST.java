3f89e5a46c400f928b6e7291621c09e62d60b1c8
davps <davidperez3010@gmail.com>
2018-03-23 00:06:26 -0300

    Document my learning resources for trie

--- src/datastructures/sorting/bydigit/TrieST.java
+++ src/datastructures/sorting/bydigit/TrieST.java
@@ -1,5 +1,34 @@
 package datastructures.sorting.bydigit;
 
+/**
+ * Implementation of the R-way trie algorithm
+ * <p>
+ * Userful learning resources I've used:
+ * <ul>
+ *   <li>
+ *   	<a href="https://algs4.cs.princeton.edu/lectures/52Tries.pdf">From Princeton, with implementation example</a>
+ *   </li>
+ *   <li>
+ *   	<a href="https://www.cs.cmu.edu/~fp/courses/15122-f10/lectures/18-tries.pdf"> From CMU</a>
+ *   </li>
+ *   <li>
+ *   	<a href="https://www.cs.cmu.edu/~avrim/451f11/recitations/rec0921.pdf"> More from CMU</a>
+ *   </li>
+ *   <li>
+ *   	<a href="https://medium.com/basecs/trying-to-understand-tries-3ec6bede0014">And this nice blog post</a>
+ *   </li>
+ * </ul>
+ *   <p>
+ *   Also, to visualize the test cases I've used a graph drawing tool called graphviz:
+ *   <br>
+ *   <br>
+ *   <a href="http://www.samsarin.com/project/dagre-d3/latest/demo/interactive-demo.html">Run online</a>
+ *   <a href="https://graphviz.gitlab.io/_pages/pdf/dotguide.pdf>Documentation</a>
+ *   
+ *   
+ * @author David Perez
+ *
+ */
 public class TrieST {
 	/**
 	 * The size of the alphabet to be supported
