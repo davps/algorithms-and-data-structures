1d43b02ea3afcb70df515986f0ffa749878dcc10
davps <davidperez3010@gmail.com>
2018-03-28 08:59:29 -0400

    Prim's algorithm implementation of Minimum Spanning Tree
    I start with comments of how it works and then evolve
    the code from there.

---
+++ src/algorithms/SpanningTree.java
@@ -0,0 +1,22 @@
+package algorithms;
+
+public class SpanningTree {
+
+	public static void main(String[] args) {
+		//
+		//Minimum spanning tree MST with Prism's algorithm
+		//
+		//Grows a MST from a starting source vertex until it spans
+		//the entire graph.
+		//Runtime: O(E*log(V)).
+		//Requires:
+		//	- A priority queue data structure with binary heap to 
+		//	  dynamically order the currently considered edges based on
+		//	  increasing weight.
+		//  - Adjacency list data structures for the fast neighbor 
+		//    enumeration of a vertex
+		//  - A boolean array to help in checking cycle.
+		
+	}
+
+}
